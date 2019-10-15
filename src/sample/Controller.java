e4f28595cb12f24d1c4f355dab774bd981d9bc3e
Guillermo Alvarez <gao090298@gmail.com>
2019-10-15 00:23:03 -0500

    ADDING OPERATION BUTTONS fxml BY MEMO

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,7 @@
 package sample;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
@@ -12,14 +13,19 @@ import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
     @FXML
-    JFXButton btnADD,btnDELETE,btnEDIT,btnSEARCH;
-
+    JFXButton btnADD,btnDELETE,btnEDIT,btnSEARCH,btnAND,btnCOND,btnOR,btnNEG;
+    @FXML
+    JFXTextField txtADD;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         btnADD.setOnAction(handler);
         btnDELETE.setOnAction(handler);
         btnEDIT.setOnAction(handler);
         btnSEARCH.setOnAction(handler);
+        btnAND.setOnAction(handler);
+        btnOR.setOnAction(handler);
+        btnCOND.setOnAction(handler);
+        btnNEG.setOnAction(handler);
     }
     EventHandler<ActionEvent> handler= new EventHandler<ActionEvent>() {
         @Override
@@ -39,6 +45,23 @@ public class Controller implements Initializable {
             if(event.getSource()==btnSEARCH)
             {
 
+            }
+            if(event.getSource()==btnOR)
+            {
+                txtADD.setText(txtADD.getText()+"V");
+
+            }
+            if(event.getSource()==btnAND)
+            {
+                txtADD.setText(txtADD.getText()+"^");
+            }
+            if(event.getSource()==btnCOND)
+            {
+                txtADD.setText(txtADD.getText()+"->");
+            }
+            if(event.getSource()==btnNEG)
+            {
+                txtADD.setText(txtADD.getText()+"¬");
             }
         }
     };
