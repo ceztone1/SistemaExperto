b114e8b4867b7e67b0b4e5c6e47e202069cd0dc2
Quiqueitc <luiserp86@gmail.com>
2019-10-14 22:00:50 -0500

    UPDATE VIEW fxml by QUIQUE

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1000, 800));
+        Parent root = FXMLLoader.load(getClass().getResource("mainView.fxml"));
+        primaryStage.setTitle("Expert system");
+        Scene scene=new Scene(root);
+        primaryStage.setMaximized(true);
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
